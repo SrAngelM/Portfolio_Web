af648d73235a2ce40c5ffe49c73293c8f7b33dc9
Angel <angelmontc27@gmail.com>
2023-04-10 02:05:29 -0400

    Commit final

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,15 @@ const menuHamburger = document.querySelector("#hamburguer");
 const navMenu = document.querySelector(".navbar-menu");
 
 menuHamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("navbar-menu__active");
+  if (navMenu.style.marginLeft === "-100%") {
+    navMenu.style.marginLeft = "0";
+  } else {
+    navMenu.style.marginLeft = "-100%";
+  }
+});
+
+navMenu.querySelectorAll("a").forEach((item) => {
+  item.addEventListener("click", (e) => {
+    navMenu.style.marginLeft = "-100%";
+  });
 });
